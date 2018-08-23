1b3bb2a9b9375f71cc76b2c9789cd8c71bd1b4b6
虞四明 <13966931523@163.com>
2018-08-23 17:10:53 +0800

    比较插入排序与希尔排序的性能

--- LearningAlgorithms/src/chapter2/SortCompare.java
+++ LearningAlgorithms/src/chapter2/SortCompare.java
@@ -2,6 +2,7 @@ package chapter2;
 
 import chapter1.section4.StopWatch;
 import chapter2.section1.Selection;
+import chapter2.section1.Shell;
 import edu.princeton.cs.algs4.StdRandom;
 
 /**
@@ -17,7 +18,7 @@ public class SortCompare {
      * @param: [sortMethod, a]
      * @return: double
      */
-    public static double time(String sortMethod, Comparable[] a) {
+    private static double time(String sortMethod, Comparable[] a) {
         StopWatch stopWatch = null;
         if (sortMethod.equals("Selection")) {
             stopWatch = new StopWatch();
@@ -25,6 +26,9 @@ public class SortCompare {
         } else if (sortMethod.equals("Insertion")) {
             stopWatch = new StopWatch();
             Selection.sort(a);
+        } else if (sortMethod.equals("Shell")) {
+            stopWatch = new StopWatch();
+            Shell.sort(a);
         }
         assert stopWatch != null;
         return stopWatch.elapsedTime();
@@ -37,7 +41,7 @@ public class SortCompare {
      * @param: [sortMethod, N, T]
      * @return: double
      */
-    public static double timeRadomInput(String sortMethod, int N, int T) {
+    private static double timeRadomInput(String sortMethod, int N, int T) {
         double totalTime = 0.0;
         Double[] a = new Double[N];
         for (int i = 0; i < T; i++) {
@@ -50,20 +54,13 @@ public class SortCompare {
     }
 
     public static void main(String[] args) {
+        double sum = 0.0;
         for (int i = 0; i < 10; i++) {
-            double selectionTime = timeRadomInput("Selection", 1000, 1000);
-            double InsertionTIme = timeRadomInput("Insertion", 1000, 1000);
+            double selectionTime = timeRadomInput("Insertion", 1600, 1000);
+            double InsertionTIme = timeRadomInput("Shell", 1600, 1000);
             System.out.println(selectionTime/InsertionTIme);
+            sum += selectionTime/InsertionTIme;
         }
-        //1.9980256663376086
-        // 0.9575871819038642
-        // 0.9863680623174296
-        // 1.0009881422924902
-        // 0.9557788944723618
-        // 0.9989816700610998
-        // 1.0706751054852321
-        // 0.9851632047477745
-        // 1.0274111675126902
-        // 1.014807502467917
+        System.out.println("平均时间：" + sum/10);
     }
 }

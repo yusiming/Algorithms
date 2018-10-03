0de0c758f0ec674df1a68b7e2bbbd802e9fa8dd5
虞四明 <13966931523@163.com>
2018-10-03 14:45:55 +0800

    算法模板

---
+++ LearningAlgorithms/src/chapter4/section1/Search.java
@@ -0,0 +1,33 @@
+package chapter4.section1;
+
+/**
+ * @Auther: yusiming
+ * @Date: 2018/10/1 17:25
+ */
+public class Search {
+    /**
+     * 找到图graph中，所有和s顶点连通的顶点
+     */
+    public Search(Graph graph, int s) {
+
+    }
+
+    /**
+     * 判断顶点s和顶点v是否是联通的
+     *
+     * @param v 顶点
+     */
+    public boolean marked(int v) {
+        return false;
+    }
+
+    /**
+     * 返回与顶点s连通的顶点数量
+     *
+     * @return 顶点个数
+     */
+    public int count() {
+        return 0;
+    }
+}
+

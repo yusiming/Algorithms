ea87f015751a7899df28b7d66fa06e34eb3b2757
虞四明 <13966931523@163.com>
2018-10-10 16:51:45 +0800

    代码规范化

--- LearningAlgorithms/src/chapter1/section3/FixedCapacityStackOfStrings.java
+++ LearningAlgorithms/src/chapter1/section3/FixedCapacityStackOfStrings.java
@@ -1,48 +1,60 @@
 package chapter1.section3;
 
 /**
- * @Auther: yusiming
- * @Date: 2018/8/16 10:40
- * @Description: 使用数组实现定容String栈
+ * 使用数组实现定容String栈
+ *
+ * @Auther yusiming
+ * @Date 2018/8/16 10:40
  */
 public class FixedCapacityStackOfStrings {
-    // 使用一个数组来保存元素
+    /**
+     * 使用数组存储数据
+     */
     private String[] a;
-    // N为数组中元素的个数，即栈的长度
-    private int N = 0;
+    /**
+     * 使用N表示栈中字符串的个数，同时也充当了栈顶的索引
+     */
+    private int N;
 
-    public FixedCapacityStackOfStrings() {
-    }
-
-    // 初始化数组的大小
+    /**
+     * 创建一个固定大小的栈
+     *
+     * @param cap 栈的大小
+     */
     public FixedCapacityStackOfStrings(int cap) {
         a = new String[cap];
+        this.N = 0;
     }
 
-    // 判断栈是否为空
+    /**
+     * 向栈中添加一个字符串
+     *
+     * @param s 被添加的字符串
+     */
+    public void push(String s) {
+        a[N++] = s;
+    }
+
+    /**
+     * @return 返回最近添加的一个元素
+     */
+    public String pop() {
+        return a[--N];
+    }
+
+    /**
+     * @return 若栈为空，返回true，否则返回false
+     */
     public boolean isEmpty() {
         return N == 0;
     }
 
-    // 判断栈是否已满
-    public boolean isFull() {
-        return N == a.length;
-    }
-
-    // 返回栈的大小
+    /**
+     * 返回栈中的元素的个数
+     *
+     * @return 元素的个数
+     */
     public int size() {
         return N;
-    }
-
-    // 向栈中添加元素
-    public void push(String item) {
-        // 将item添加到数组a末尾，同时将栈的长度加一
-        a[N++] = item;
-    }
-
-    // 弹出栈中的元素
-    public String pop() {
-        // 弹出元素,同时将栈的大小减一
-        return a[--N];
     }
 }

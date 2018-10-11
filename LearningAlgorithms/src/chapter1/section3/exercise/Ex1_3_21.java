74a68c7378742f5b664affedad015ba8d9f54935
虞四明 <13966931523@163.com>
2018-10-11 17:41:28 +0800

    链表练习之判断链表中是否存在指定元素

--- LearningAlgorithms/src/chapter1/section3/exercise/Ex1_3_21.java
+++ LearningAlgorithms/src/chapter1/section3/exercise/Ex1_3_21.java
@@ -1,32 +1,58 @@
 package chapter1.section3.exercise;
 
 /**
- * @Auther: yusiming
- * @Date: 2018/8/17 15:51
- * @Description:
+ * 判断链表中是否存在指定元素t
+ *
+ * @Auther yusiming
+ * @Date 2018/10/11 17:27
  */
-public class Ex1_3_21 {
-    public static void main(String[] args) {
-        Node node = new Node();
-        for (int i = 0; i < 5; i++) {
-            Node oldNode = node;
-            node = new Node();
-            node.t = i;
-            node.next = oldNode;
-        }
-        System.out.println(find(node,5));
-    }
-    private static class Node<T> {
+public class Ex1_3_21<T> {
+    /**
+     * 首结点
+     */
+    private Node first;
+
+    private class Node {
         T t;
         Node next;
     }
 
-    public static boolean find(Node first, int k) {
-        for (Node x = first; x.next != null; x = x.next) {
-            if (x.t == Integer.valueOf(k)) {
+    /**
+     * 添加元素到链表中
+     *
+     * @param t 元素的值
+     */
+    public void addNode(T t) {
+        Node oldFirst = first;
+        first = new Node();
+        first.t = t;
+        first.next = oldFirst;
+    }
+
+    /**
+     * 判断链表中是否存在元素t
+     *
+     * @param node 链表
+     * @param t    元素
+     * @return 若存在，返回true，若不存在返回false
+     */
+    public boolean find(Node node, T t) {
+        while (node != null) {
+            if (node.t == t) {
                 return true;
             }
+            node = node.next;
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Node x = first; x != null; x = x.next) {
+            stringBuilder.append(x.t);
+            stringBuilder.append(' ');
+        }
+        return stringBuilder.toString();
     }
 }
